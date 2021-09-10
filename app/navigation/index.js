326fa383bc0c87c9295ae0dfdf48665369e311a3
ivanroh <ivanrodrigo24@gmail.com>
2021-09-11 00:28:31 +0200

    Quitado header de Home

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -3,18 +3,20 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 
-
 const Drawer = createDrawerNavigator();
 
-
 function Navigation() {
-    return (
-      <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name="Home" component={Home} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
-  }
-  
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default Navigation;
